73729b592cbd471ff5cc5fbe4a31433075e712dd
nika fartenadze <nikafartenadze@nikas-MacBook-Pro.local>
2024-03-05 10:22:06 +0400

    refactor: class names based on convention

--- homeworks/nika-fartenadze/02-css/index.js
+++ homeworks/nika-fartenadze/02-css/index.js
@@ -1,4 +1,4 @@
-const themeToggle = document.querySelector(".theme-toggle-button");
+const themeToggle = document.querySelector(".theme-toggle-container__button");
 
 themeToggle.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
