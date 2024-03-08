ed6af58b4f73a53f177f0bff45266750faae486d
ramiro <r.miguez.lauria@gmail.com>
2024-03-08 03:57:09 -0300

    removed log

--- homeworks/ramiro-miguez/03-css-advanced/colorswitchmode.js
+++ homeworks/ramiro-miguez/03-css-advanced/colorswitchmode.js
@@ -1,6 +1,5 @@
 const toggleThemeButton = document.getElementById("theme-toggle");
 
 toggleThemeButton.addEventListener("click", () => {
-  console.log("lol");
   document.body.classList.toggle("light-mode");
 });
