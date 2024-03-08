eecb74ac62040379b61d92025d94b5362efb6913
jparaducha <paraducha_97@hotmail.com>
2024-03-07 22:37:08 -0300

    dark mode

--- homeworks/juan-paraducha/02-css/script.js
+++ homeworks/juan-paraducha/02-css/script.js
@@ -1,11 +1,20 @@
 function toggleButton(clickedButtonId) {
     var buttons = document.querySelectorAll('#portfolio, #skills');
-    
+    var body = document.body;
+
     buttons.forEach(function(button) {
-        if (button.id === clickedButtonId) {
-            button.style.backgroundColor = 'lightgrey'; 
+        if (body.classList.contains("dark-mode")) {
+            button.style.backgroundColor = '#333'; // Dark mode background color
         } else {
-            button.style.backgroundColor = 'white';
+            button.style.backgroundColor = 'white'; // Light mode background color
+        }
+
+        if (button.id === clickedButtonId) {
+            button.style.backgroundColor = 'lightgrey'; // Highlight the clicked button
         }
     });
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
 }
