fa75d9424b3defc395f70fd4b11cf11c4d1b59c7
TatsianaLohinava <tlohinava@solvd.com>
2024-03-28 11:36:12 +0300

    feat: add and update avatars

--- homeworks/tatsiana-lohinava/avatar-app/src/App.js
+++ homeworks/tatsiana-lohinava/avatar-app/src/App.js
@@ -1,9 +1,38 @@
+import { useState } from 'react';
 import './App.css';
+import Tile from './Tile';
+import NewPhoto from './NewPhoto';
+import { API_URL } from './data';
 
 function App() {
+  const [photoBase, setPhotoBase] = useState([])
+
+  const fetchPhoto = async () => {
+    try {
+      const response = await fetch(API_URL);
+      const json = await response.json();
+      return json;
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  const addNewPhoto = async () => {
+    const [photo] = await fetchPhoto();
+    setPhotoBase([...photoBase, photo])
+  };
+
+  const updatePhoto = async (index) => {
+    const [photo] = await fetchPhoto();
+    setPhotoBase([...photoBase.slice(0, index), photo, ...photoBase.slice(index + 1)])
+  }
+
   return (
     <div className="App">
-
+      <div className='tile-container'>
+        {photoBase.map((item, index) => <Tile data={item} handleDataUpdate={() => updatePhoto(index)} key={item.id} />)}
+        <NewPhoto handleClick={addNewPhoto} />
+      </div>
     </div>
   );
 }
