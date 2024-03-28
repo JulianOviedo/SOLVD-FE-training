36ac36e593bd717790c84426fe3a0d66ce2776c9
TatsianaLohinava <tlohinava@solvd.com>
2024-03-28 15:05:50 +0300

    feat: add general update option

--- homeworks/tatsiana-lohinava/avatar-app/src/App.js
+++ homeworks/tatsiana-lohinava/avatar-app/src/App.js
@@ -4,13 +4,15 @@ import Tile from './Tile';
 import NewPhoto from './NewPhoto';
 import { API_URL } from './data';
 
+const refreshAllText = 'Refresh all';
+
 function App() {
   const [photoBase, setPhotoBase] = useState([])
 
   const fetchPhoto = async () => {
     try {
       const response = await fetch(API_URL);
-      const json = await response.json();
+      const [json] = await response.json();
       return json;
     } catch (error) {
       console.log("error", error);
@@ -18,13 +20,18 @@ function App() {
   };
 
   const addNewPhoto = async () => {
-    const [photo] = await fetchPhoto();
+    const photo = await fetchPhoto();
     setPhotoBase([...photoBase, photo])
   };
 
   const updatePhoto = async (index) => {
-    const [photo] = await fetchPhoto();
+    const photo = await fetchPhoto();
     setPhotoBase([...photoBase.slice(0, index), photo, ...photoBase.slice(index + 1)])
+  }
+
+  const updateAll = async () => {
+    const newBase = await Promise.all(photoBase.map(async () => await fetchPhoto()));
+    setPhotoBase(newBase)
   }
 
   return (
@@ -33,6 +40,7 @@ function App() {
         {photoBase.map((item, index) => <Tile data={item} handleDataUpdate={() => updatePhoto(index)} key={item.id} />)}
         <NewPhoto handleClick={addNewPhoto} />
       </div>
+      <button className='tile-updater' onClick={updateAll}>{refreshAllText}</button>
     </div>
   );
 }
